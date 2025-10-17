52da53539df89c82317864350d05167b45cd5f05
taewonKang1 <edward0317@naver.com>
2025-10-17 15:22:44 +0900

    gitapi app created

--- githubapp/src/App.tsx
+++ githubapp/src/App.tsx
@@ -1,11 +1,43 @@
 import { useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
+type Repository = {
+  id: number
+  full_name: string
+  html_url: string
+};
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [ keyword, setKeyword ] = useState('');
+  const [ repodata, setRepodata ] = useState<Repository[]>([]);
+  
+  const handleClick = () => {
+    axios.get<{ items : Repository[] }>(`https://api.github.com/search/repositories?q=${keyword}`)
+    .then(response => setRepodata(response.data.items))
+    .catch(error => console.log(error))
+  }
 
   return (
     <>
+      <input type="text" onChange={e => setKeyword(e.target.value)} value={keyword}/>
+      <button onClick={handleClick}>search</button>
+      {repodata.length === 0 ? (
+        <p>No data available</p>
+      ) : (
+        <table>
+          <tbody>
+            {repodata.map(repo => 
+              <tr key={repo.id}>
+                <td>{repo.full_name}</td>
+                <td>
+                  <a href={repo.html_url}>{repo.html_url}</a>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      )}
     </>
   )
 }
